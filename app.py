9f02829f8cde08e6a7d2f6916c617eddcfc9c5bc
1127970172@qq.com <1127970172@qq.com>
2021-09-06 21:02:15 +0800

    Add minimal home page

--- app.py
+++ app.py
@@ -4,10 +4,26 @@
 # @Author  : wangke
 
 from flask import Flask
+from flask import url_for, escape
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello():
-    return "Welcome to My Watchlist!"
+    return "Hello"
+
+
+@app.route("/user/<name>")
+def user_page(name):
+    return "User:%s" % escape(name)
+
+
+@app.route("/test")
+def test_url_for():
+    print(url_for('hello'))
+    print(url_for('user_page', name="wangke"))
+    print(url_for('user_page', name="siqi"))
+    print(url_for('test_url_for'))
+    print(url_for('test_url_for', num=2))
+    return "Test page"
